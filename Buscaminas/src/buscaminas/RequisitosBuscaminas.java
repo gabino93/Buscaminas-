f5bf0f053b9722284ab72e92f6cca74d989ad654
Multiplataforma15 <luis_garciaa16@hotmail.com>
2015-01-27 10:14:13 -0700

    requisitos de buscaminas

---
+++ Buscaminas/src/buscaminas/RequisitosBuscaminas.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package buscaminas;
+
+/**
+ *
+ * @author fimaz2014
+ */
+public class RequisitosBuscaminas {
+    /*-numero de minas 
+-marcador de tiempo
+-boton para comenzar el juego
+-cuadricula de x por de y de botones
+
+
+-boton para comenzar el juego 
+     reiniciar el juego
+
+
+-cuadricula de x por y de botones  
+
+
+-al dar click en un botón revisa cuantas minas existen de manera adyacente al botón presionado.
+
+
+x=5
+y=5
+    */
+}
